2bc094a8cc5c17e0464685428d1603d5fa8795d7
Pranab321 <pranab.success@gmail.com>
2023-01-07 22:37:47 +0530

    End of section 3

---
+++ NZWalks/NZWalksAPI/Controllers/RegionsController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using NZWalksAPI.Models.Domain;
+using NZWalksAPI.Repositories;
+using System.Runtime.InteropServices;
+
+namespace NZWalksAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository _regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this._regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegions()
+        {
+            //var regions = new List<Region>()
+            //{
+            //    new  Region
+            //    {
+            //      Id = Guid.NewGuid(),
+            //      Name="Wellington",
+            //      Code="WLG",
+            //      Area=222555,
+            //      Lat=102.222,
+            //      Long=235.25,
+            //      Population=500000
+            //    }, 
+            //    new  Region
+            //    {
+            //      Id = Guid.NewGuid(),
+            //      Name = "Auckland",
+            //      Code = "AULK",
+            //      Area = 345345,
+            //      Lat = 45.254,
+            //      Long = 235.25,
+            //      Population = 500000
+            //    }
+            //};
+            var regions = await _regionRepository.GetAlAsyncl();
+
+            //return DTO regions
+
+            //var regionsDTO = new List<Models.DTO.Region>();
+            //regions.ToList().ForEach(regions =>
+            //{
+            //    var regionDTO = new Models.DTO.Region()
+            //    {
+            //        Id=regions.Id,
+            //        Code=regions.Code,  
+            //        Name=regions.Name,  
+            //        Lat=regions.Lat,
+            //        Long=regions.Long,
+            //        Area=regions.Area,
+            //        Population=regions.Population
+
+            //    };
+            //    regionsDTO.Add(regionDTO);  
+            //});
+
+            var regionsDTO =  mapper.Map<List<Models.DTO.Region>>(regions);
+            return Ok(regionsDTO);
+
+        }
+
+    }
+}
---
+++ NZWalks/NZWalksAPI/Repositories/RegionRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Data;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDBContext nZWalksDBContext1;
+        public RegionRepository(NZWalksDBContext nZWalksDBContext)
+        {
+            this .nZWalksDBContext1 = nZWalksDBContext; 
+        }
+        public async Task<IEnumerable<Region>> GetAlAsyncl()
+        {
+            return await nZWalksDBContext1.Regions.ToListAsync();
+        }
+    }
+}
